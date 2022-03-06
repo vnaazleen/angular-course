c5d13f9a0f1ca1cc194b7baa2f55b38182c1e884
vnaazleen <shaikvaseemnaazleen@gmail.com>
2022-03-06 12:16:23 +0530

    Added color

--- assignment1/app.js
+++ assignment1/app.js
@@ -4,22 +4,25 @@
     angular.module('LunchCheck', [])
 
     .controller('LunchCheckController', LunchCheckController);
-    
+
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
         $scope.message=""
         $scope.menu = ""
         $scope.color = ""
+        $scope.borderColor= "";  
 
 
         $scope.checkMenu = function () {
             if($scope.menu == "") {
-                $scope.color = "red"
+                $scope.color = {"color":"red"}
+                $scope.borderColor={"border-color":"red"};  
                 $scope.message = "Please enter data first";
                 return;
             }
 
-            $scope.color = "green"
+            $scope.color = {"color":"green"}
+            $scope.borderColor={"border-color":"green"};  
             var totalItems = $scope.menu.split(",").length;
             if(totalItems <= 3) {
                 $scope.message = "Enjoy!";
